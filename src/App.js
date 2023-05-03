91389ed3815af12a62c264570715be3f535ec4a8
Rishabh Singh <rishimark98@gmail.com>
2023-05-03 21:48:21 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/features" element={<h2>Features Page <a href="/details">details</a></h2>} />
+      <Route path="/details" element={<h2>Details Page <a href="/details/profile">profile</a></h2>} />
+      <Route path="/details/profile" element={<h2>Profile Page <a href="/">Home</a></h2>} />
+      <Route
+        path="/"
+        element={
+          <h2>
+            Home Page <a href="/features">features</a>
+          </h2>
+        }
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
